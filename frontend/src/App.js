3a75742e9f56d8337bb8575c7c6ac12e62f8a938
Alpha-4 <aarondadhwal@gmail.com>
2025-08-27 21:25:47 +0530

    did some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import idl from "./idl.json";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Program, AnchorProvider, web3, utils, BN } from "@coral-xyz/anchor";
+//import Buffer from "buffer";
+
+const programId = new PublicKey(idl.address);
+const network = clusterApiUrl("devnet");
+const opts = {
+  preflightCommitment: "processed",
+};
+const { SystemProgram } = web3;
 
 const App = () => {
-  const [walletAddress, setWalletAddress] = useState(null);
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new AnchorProvider(
+      connection,
+      window.solana,
+      opts.preflightCommitment
+    );
+    return provider;
+  };
+
   const checkWalletIsConnected = async () => {
     const { solana } = window;
     if (solana?.isPhantom) {
@@ -26,6 +46,38 @@ const App = () => {
     console.log("connect public key::", response.publicKey.toString());
   };
 
+  const createCampaign = async () => {
+    const provider = getProvider();
+    const program = new Program(idl, programId, provider);
+
+    console.log(program.account);
+    const [campaign] = await PublicKey.findProgramAddressSync(
+      [
+        utils.bytes.utf8.encode("CAMPAIGN_DEMO"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    // Generate a new campaign account
+    const campaignKeypair = web3.Keypair.generate();
+
+    //call to create
+    await program.methods
+      .initialize("FundME", "Description of FundMe")
+      .accounts({
+        campaign,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([campaignKeypair]) // campaign must sign since it's being created
+      .rpc();
+
+    console.log("crated campaign::", campaign);
+  };
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
   useEffect(() => {
     const onLoad = async () => {
       await checkWalletIsConnected();
@@ -34,13 +86,28 @@ const App = () => {
     return () => window.removeEventListener("load", onLoad);
   }, []);
 
-  const button = () => {
-    <div>
-      <button onClick={() => connectToWallet()}>Connect Phantom</button>
-    </div>;
+  const connectButton = () => {
+    return (
+      <div>
+        <button onClick={() => connectToWallet()}>Connect Phantom</button>
+      </div>
+    );
   };
 
-  return <>{!walletAddress && button}</>;
+  const createCampaignButton = () => {
+    return (
+      <div>
+        <button onClick={() => createCampaign()}>create Campaign</button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      {!walletAddress ? connectButton() : createCampaignButton()}
+    </div>
+  );
 };
 
 export default App;
